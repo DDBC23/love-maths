8be5bea505b1ef3c2372e2feeb5a22ee7fd222cf
DDBC23 <danielbc23@hotmail.com>
2022-03-26 16:13:14 +0000

    Add division question code

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 return [operand1 * operand2, "multiply"];
             } else if(operator === "-") {
                 return [operand1 - operand2, "subtract"];
-            } else if(operator === "%") {
+            } else if(operator === "/") {
                 return [operand1 % operand2, "division"];
             } else {
                 let gameType = this.getAttribute("data-type");
@@ -92,8 +92,8 @@ function calculateCorrectAnswer() {
         return [operand1 * operand2, "multiply"];
     } else if(operator === "-") {
         return [operand1 - operand2, "subtract"];
-    } else if(operator === "%") {
-        return [operand1 % operand2, "division"];
+    } else if(operator === "/") {
+        return [operand1 / operand2, "division"];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -145,9 +145,10 @@ function displayMultiplyQuestion(operand1, operand2) {
 
 // Division question
 function displayDivisionQuestion(operand1, operand2) {
+    operand1 = operand1 * operand2;
 
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
-    document.getElementById('operator').textContent = "%";
+    document.getElementById('operator').textContent = "/";
 }
 
